8cbd9a91dcffe89e5b6932c547a7366801c18f42
Dmitriy Kunin <dkunin@avito.ru>
2024-12-12 16:38:15 +0300

    Update

--- src/pages/HomePage/HomePage.ts
+++ src/pages/HomePage/HomePage.ts
@@ -9,11 +9,10 @@ export class HomePage {
 
   constructor(context: AppContext) {
     const { launchParams: lp } = context;
-    console.log(lp.initData?.user);
     this.page = new Page({ title: "" }).appendChild(
       $("<h2 >Добро пожаловать, " + lp.initData?.user?.username + "</h2>"),
       lp.initData?.user?.photoUrl
-        ? $(`<img src="${lp.initData?.user?.photoUrl}"/>`)
+        ? $(`<img class="round-avatar" src="${lp.initData?.user?.photoUrl}"/>`)
         : "",
       $(
         '<a class="button-link centered-button" href="http://185.251.90.61:3939/learnAuth?login=true">Войти</a>'
