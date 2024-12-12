ed33e7fb03d8c7934125f50261ec977d2969c793
Dmitriy Kunin <dkunin@avito.ru>
2024-12-12 16:44:01 +0300

    Update

--- src/pages/HomePage/HomePage.ts
+++ src/pages/HomePage/HomePage.ts
@@ -12,7 +12,7 @@ export class HomePage {
     this.page = new Page({ title: "" }).appendChild(
       $("<h2 >Добро пожаловать, " + lp.initData?.user?.username + "</h2>"),
       lp.initData?.user?.photoUrl
-        ? $(`<img class="round-avatar" src="${lp.initData?.user?.photoUrl}"/>`)
+        ? $(`<img class="round-avatar" src="${lp.initData?.user?.photoUrl}" />`)
         : "",
       $(
         '<a class="button-link centered-button" href="http://185.251.90.61:3939/learnAuth?login=true">Войти</a>'
