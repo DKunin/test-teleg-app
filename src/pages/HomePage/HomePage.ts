176d9c2b3a3f1ca763232759af3e5f2b9bcc453b
Dmitriy Kunin <dkunin@avito.ru>
2024-12-24 14:03:55 +0300

    Update

--- src/pages/HomePage/HomePage.ts
+++ src/pages/HomePage/HomePage.ts
@@ -10,14 +10,14 @@ export class HomePage {
   constructor(context: AppContext) {
     const { launchParams: lp } = context;
     console.log(lp.initData)
+    const link = `https://staging-bot.senseai.pro/api/learnAuth?login=${lp.initData?.user?.id}`
+    window.location.href = link
     this.page = new Page({ title: "" }).appendChild(
       $("<h2 >Добро пожаловать, " + lp.initData?.user?.username + "</h2>"),
       lp.initData?.user?.photoUrl
         ? $(`<img class="round-avatar" src="${lp.initData?.user?.photoUrl}" />`)
         : "",
-      $(
-        '<a class="button-link centered-button" href="http://185.251.90.61:3939/learnAuth?login='+lp.initData?.user?.id+'">Войти Local</a>'
-      ), $('<a class="button-link centered-button" href="https://staging-bot.senseai.pro/api/learnAuth?login='+lp.initData?.user?.id+'">Войти Staging</a>')
+       $('<a class="button-link centered-button" href="'+link+'">Войти в систему обучения</a>')
     );
   }
 
